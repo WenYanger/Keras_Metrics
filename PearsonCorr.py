29358a18cb0c50dafc3ff3e9f87a6d11c1a6767c
WenYanger <516015029@qq.com>
2018-05-31 18:53:59 +0800

    Create PearsonCorr.py
    Pearsons Correlation Coefficient

---
+++ PearsonCorr.py
@@ -0,0 +1,13 @@
+from keras import backend as K
+def pearson_r(y_true, y_pred):
+    x = y_true
+    y = y_pred
+    mx = K.mean(x, axis=0)
+    my = K.mean(y, axis=0)
+    xm, ym = x - mx, y - my
+    r_num = K.sum(xm * ym)
+    x_square_sum = K.sum(xm * xm)
+    y_square_sum = K.sum(ym * ym)
+    r_den = K.sqrt(x_square_sum * y_square_sum)
+    r = r_num / r_den
+    return K.mean(r)
